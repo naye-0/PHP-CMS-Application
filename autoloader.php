d621c5d2d9953105d155c2fc28a3f93de7110a95
naye-0 <nasidkayevhenii@gmail.com>
2026-02-09 19:56:47 +0100

    add Autoload classes from the "classes" directory when they are instantiated

---
+++ autoloader.php
@@ -0,0 +1,13 @@
+<?php
+
+// Autoload classes from the "classes" directory when they are instantiated
+spl_autoload_register(function($class_name) {
+    $directory = __DIR__ . "/classes/";
+    $file = $directory . $class_name . ".php";
+
+    if(file_exists($file)){
+        require_once $file;
+    } else {
+        die("Class file for {$class_name} not found in {$file}");
+    }
+});
--- init.php
+++ init.php
@@ -2,11 +2,14 @@
 // Start Session
 session_start();
 
+// Autoload classes
+require_once "autoloader.php";
+
 // Include the main configuration file
 require_once "config/config.php";
 
 // Load Database
-require_once "classes/Database.php";
+// require_once "classes/Database.php";
 
 // Include helper function
 require_once "helpers.php";
